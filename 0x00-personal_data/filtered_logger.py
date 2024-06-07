046519d2bf9cb7c2438257f666b9b4c5348eab9f
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-06-07 02:43:33 +0100

    refactor: setting logger Format is done by using setFormatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -46,6 +46,6 @@ def get_logger() -> logging.Logger:
     logger  = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     handler = logging.StreamHandler()
-    handler.setFormat(RedactingFormatter(PII_FIELDS))
+    handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
     logger.addHandler(handler)
     return (logger)
