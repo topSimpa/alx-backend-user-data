55e89632571f4bd0d97bdfd4febd802368e52ba7
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-06-14 04:05:18 +0100

    add: added the create_session method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -51,3 +51,13 @@ class Auth:
                 'ascii'), pot_user.hashed_password)
         except Exception:
             return False
+
+    def create_session(self, email: str) -> str:
+        """creates a new session for a user"""
+
+        try:
+            user = self._db.find_user_by(email=email)
+            self._db.update_user(user.id, session_id=_generate_uuid())
+            return (user.session_id)
+        except NoResultFound:
+            pass
