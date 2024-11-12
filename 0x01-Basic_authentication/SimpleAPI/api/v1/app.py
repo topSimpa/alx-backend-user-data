e679ba64300ae0946d4cfae2af9c97a49756d239
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-11-12 19:49:58 +0100

    fix: fixed the style for the python

--- 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
@@ -20,11 +20,12 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-   """ Unauthorized request
-   """
-   return jsonify({"error": "Unauthorized"}), 401
+    """ Unauthorized request
+    """
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
