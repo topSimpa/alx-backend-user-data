83cc2e84b10d6c75a9c36fa8c1c1eadf20c1772a
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-11-14 00:07:53 +0100

    update basic_auth.py: added extract_base64_authorization method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
-""" Module for Basic_auth implementation """
+""" Module for Basic_auth implementation
+"""
 
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """Basic Auth Authentication implementation for Api"""
-    pass
+    """ Basic Auth Authentication implementation for Api
+    """
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """Extracts the base64 part of Authorization header
+        Return:
+          - None: if authorization_header is None
+                  if authorization_header is not a string
+                  if authorization_header does not start with 'Basic '
+            str: value after Basic in authorization_header
+        """
+        if authorization_header:
+            if isinstance(authorization_header, str):
+                if authorization_header[:6] == 'Basic ':
+                    return (authorization_header.replace('Basic ', ''))
