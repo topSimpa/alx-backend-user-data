9247ff7c35c9eb729101aa27f2f131e163df0a42
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-11-13 17:37:42 +0100

    create basic_auth.py: added basic_auth.py

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Module for Basic_auth implementation """
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic Auth Authentication implementation for Api"""
+    pass
