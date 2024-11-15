9608bf34b1c0a2b60d99c2b19e5e4c04c092341b
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-11-15 03:33:18 +0100

    update session_auth.py: added user_id_for_session_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -13,7 +13,7 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """Creates a session id for a user_id
+        """ Creates a session id for a user_id
         Return:
             - None: if user_id is None
                     if user_id is not a string
@@ -24,3 +24,14 @@ class SessionAuth(Auth):
                 session_id = uuid4()
                 self.user_id_by_session_id[session_id] = user_id
                 return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Get a user_id by using the session_id
+        Return:
+            - None: if session_id is None
+                    if session_id is not a string
+            - str: the user_id which corresponds to the session_id
+        """
+        if session_id:
+            if isinstance(session_id, str):
+                return self.user_id_by_session_id.get(session_id)
