77e94d03e3f9c32f2fe693523df7ec9fef8c4e15
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-11-16 10:21:30 +0100

    update app.py: corrected omission of auth_type

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+auth_type = getenv("AUTH_TYPE")
 if auth_type:
     if auth_type == 'auth':
         from api.v1.auth.auth import Auth
