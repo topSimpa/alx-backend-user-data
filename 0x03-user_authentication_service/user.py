9f47fce5d3e6084f14d90c3f30d844055dd485f7
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-11-18 21:30:58 +0100

    create user: added the user mapping for users table

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""User Model Module"""
+""" Module for SQLAlchemy User model
+"""
+
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
@@ -9,9 +11,9 @@ Base = declarative_base()
 
 
 class User(Base):
-    """class for the user model"""
-
-    __tablename__ = "users"
+    """ User base class mapped to the user table
+    """
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
