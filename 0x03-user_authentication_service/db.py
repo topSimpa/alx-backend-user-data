5c2c5db75409b61c8793bac4191fbe9ea6f9055d
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-11-20 13:03:03 +0100

    update db.py: added the find_user_by method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,7 +33,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password) -> TypeVar('User'):
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
         """add and save a user to the database
         Return:
             - User: User object created
