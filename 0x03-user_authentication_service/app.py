6373bc7214ac778ad00a247e066e01e44b613736
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-11-21 02:32:42 +0100

    update app.py: added the registration route /users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,12 @@
 """ Flask Application Module
 """
 
-from flask import Flask, jsonify
+from auth import Auth
+
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
@@ -16,6 +19,28 @@ def root():
        - message: Bienvenue
     """
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route('/users', methods=['POST'])
+def register() -> str:
+    """ POST /users
+    Return:
+        - JSON: containing email and a sucess message, 200
+        - JSON; if user already exist, returns a failure message, 400
+    Body:
+        - email (only on sucess): <registered email>
+        - message (on sucess): user created
+                  (on failure): email already registered
+    """
+    email = request.form['email']
+    password = request.form['password']
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": "{}".format(email),
+                       "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
