d9cf9b1f0219816882d3dd134d6e6de19e9f6120
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-11-22 05:34:49 +0100

    update auth.py: added get_reset_password_token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -97,3 +97,15 @@ class Auth:
         """
         user = self._db.find_user_by(id=user_id)
         self._db.update_user(user.id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Generate a reset password token
+        Return:
+            - str: the password token generated
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            self._db.update_user(user.id, reset_token=_generate_uuid())
+            return user.reset_token
+        except BaseException:
+            raise ValueError
