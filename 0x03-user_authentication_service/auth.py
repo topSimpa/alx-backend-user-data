21076862bad5d717237ec1371b75bbd2834d9b4e
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-11-22 07:07:17 +0100

    update auth, app: added functionalities to update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -118,6 +118,7 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            self._db.update_user(user.id, hashed_password=_hash_password(password))
+            self._db.update_user(
+                user.id, hashed_password=_hash_password(password))
         except BaseException:
             raise ValueError
