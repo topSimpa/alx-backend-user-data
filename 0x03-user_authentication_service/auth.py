e09b6f1db3426aa4a57a4013ca3062c69dd405af
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-11-22 02:50:29 +0100

    update auth.py: added destroy_session method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -95,4 +95,4 @@ class Auth:
             - None: in all cases
         """
         user = self._db.find_user_by(id=user_id)
-        self._db.update_user(user_id, session_id=None)
+        self._db.update_user(user.id, session_id=None)
