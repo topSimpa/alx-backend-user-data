082324be3f83ced5441d5bf03cd009f55a277c54
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-11-24 07:36:13 +0100

    update session_db_auth.py: all should implement super

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -4,6 +4,7 @@
 
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
+from datetime import datetime, timedelta
 
 
 class SessionDBAuth(SessionExpAuth):
@@ -30,12 +31,13 @@ class SessionDBAuth(SessionExpAuth):
            - str: user_id if match  is success
         """
         if session_id:
-            try:
-                users = UserSession.search({"session_id": session_id})
-                if users:
-                    return users[0].user_id
-            except BaseException as e:
-                return None
+            user_id = super().user_id_for_session_id(session_id)
+            if user_id:
+                sessions = UserSession.search({"session_id": session_id})
+                if sessions:
+                    for session in sessions:
+                        if session.user_id == user_id:
+                            return user_id
 
     def destroy_session(self, request=None) -> None:
         """Destroy the userSession
@@ -43,7 +45,8 @@ class SessionDBAuth(SessionExpAuth):
              - None
         """
         if request:
+            super().destroy_session(request)
             session_id = request.cookies.get('session_id')
             users = UserSession.search({"session_id": session_id})
             if users:
-                del users[0]
+                users[0].remove()
